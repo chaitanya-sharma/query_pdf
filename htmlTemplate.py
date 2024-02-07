c86287002651658645b643cacb157376fb06f261
chaitanya-sharma <39159541+chaitanya-sharma@users.noreply.github.com>
2024-02-07 10:04:35 +0530

    Add files via upload

---
+++ htmlTemplate.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://dthezntil550i.cloudfront.net/xi/latest/xi2007262116591900016004116/1280_960/7f45d66c-6c41-422f-a945-4ce4e46dd100.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn.pixabay.com/photo/2023/03/26/07/48/ai-generated-7877747_1280.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
